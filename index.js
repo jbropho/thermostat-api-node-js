042d9dfdac1d71e1b80c9b460acddf23bf056ab2
Jordan Brooks <jordanbrooks@MacBook-Pro-68.lan>
2018-04-22 15:38:18 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+const https = require('https');
 const PORT = process.env.PORT || 5000
 
 app.use(function(req, res, next) {
@@ -8,8 +9,20 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/weather/:city', function(req, res) {
+  let city = req.params.city
+  https.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + process.env.WEATHER_KEY , (resp) => {
+    let data = '';
+
+    resp.on('data', (chunk) =>  data += chunk );
+
+    resp.on('end', () => {
+      res.send(JSON.parse(data));
+    });
+
+    }).on("error", (err) => {
+      console.log("Error: " + err.message);
+    });
 })
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
